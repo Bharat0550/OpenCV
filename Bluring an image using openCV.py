ea22713f71d3b91268a86150ce94ace8a10aac0b
Bharat singh <60250477+Bharat0550@users.noreply.github.com>
2021-07-31 03:27:45 -0500

    Update Bluring an image using openCV.py

--- Bluring an image using openCV.py
+++ Bluring an image using openCV.py
@@ -1,22 +1,22 @@
 
 # # #Bluring of a image
-# import cv2
-# import numpy as np
+import cv2
+ import numpy as np
 
-# selena_img = cv2.imread('C:\opencv\selena_gomez.jpg')
+ selena_img = cv2.imread('C:\opencv\selena_gomez.jpg')
 
-# # Smoothening of Image using kernel matrix
+ # Smoothening of Image using kernel matrix
 # #kernel = np.ones((5, 5), np.float32)/26
-# kernel = np.ones((7, 7), np.float32)/49
+ kernel = np.ones((7, 7), np.float32)/49
 
-# print(kernel)
+ print(kernel)
 
 # # #-1  depth of output image
-# smoothed_img = cv2.filter2D(selena_img, -1, kernel)
-# cv2.imshow("frame1", selena_img)
-# cv2.imshow("frame2", smoothed_img)
+ smoothed_img = cv2.filter2D(selena_img, -1, kernel)
+ cv2.imshow("frame1", selena_img)
+ cv2.imshow("frame2", smoothed_img)
 
 # # wait for any key
-# cv2.waitKey(0)
+ cv2.waitKey(0)
 # # Close all frames
-# cv2.destroyAllWindows()
+ cv2.destroyAllWindows()
