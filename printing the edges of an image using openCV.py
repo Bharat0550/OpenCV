941514f71ff73d242c12ccc21d908057fbccb685
Bharat singh <60250477+Bharat0550@users.noreply.github.com>
2021-07-31 03:16:51 -0500

    Create printing the edges of an image using openCV.py

---
+++ printing the edges of an image using openCV.py
@@ -0,0 +1,26 @@
+import cv2
+
+img=cv2.imread("C:\opencv\selena_gomez.jpg")
+print(img)
+cv2.imshow("frame1", img)
+import cv2
+
+#Getting Contour of Image using Canny Filter
+mg = cv2.imread("C:\opencv\selena_gomez.jpg")
+#Convert Image into grayscale
+gray = cv2.cvtColor(img, cv2.COLOR_BGR2GRAY)
+
+#Computing edges with Canny using thresholds 
+#Using canny filter to Contours
+edges = cv2.Canny(gray, 90, 50)
+
+high_threshold = cv2.Canny(gray, 190, 150)
+
+cv2.imshow("Original", img)
+cv2.imshow("Edges", edges)
+cv2.imshow("High Edges", high_threshold)
+
+#wait for any key
+cv2.waitKey(0)
+#Close all frames
+cv2.destroyAllWindows()
